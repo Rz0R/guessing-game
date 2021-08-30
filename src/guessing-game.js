1e69f6db8024917714a21030a2e699569791f8f5
Rz0R <vpa05@mail.ru>
2021-08-30 18:52:14 +1000

    feat: add GuessingGame function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min, max) {
+        this.min = min;
+        this.max = max;
+        this.number;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.round((this.max - this.min) / 2) + this.min;
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
